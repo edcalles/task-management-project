49b7cd6c17b67287fa10789a6e5166622e7da7a6
Edward Calles <ed.calles00@gmail.com>
2023-05-23 01:01:16 -0500

    created input fields with react :)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
+import React from "react"
 import './index.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Navbar from  "./components/Navbar";
-import Home from "./components/Home";
-import Todo from "./components/Todo";
-import Contact from "./components/Contact";
+import Navbar from  "./components/Navbar.js";
+import Home from "./components/Home.jsx";
+import Todo from "./components/Todo.jsx";
+import Contact from "./components/Contact.jsx";
+import Error from "./components/Error.jsx"
 
 export default function App(){
   return (
@@ -14,6 +16,7 @@ export default function App(){
         <Route path='/' exact component={Home} />
         <Route path='/todo' component={Todo} />
         <Route path='/contact' component={Contact} />
+        <Route component={Error} />
       </Switch>
       </div>
     </Router>
--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Prompt } from "react-router-dom";
 
-export default function Contact() {
-    return (
-        <div>
-            <h3>Contact</h3>
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ""};
+    }
+    handleChange(event){
+        this.setState({ value: event.target.value });
+    }
+    handleSubmit(){
+        this.setState({ value: "" });
+    }
+    render(){
+        var style1 = {
+            width: 300,
+            height: 150,
+            margin: 10
+        };
+        var style2 = {
+            width: 300,
+            margin: 10
+        }
+        return (
             <div>
-                Please don't contact me
+                <h2>Send me a silly message!</h2>
+                <input type="text" name="inputName" value={this.state.inputName} onChange={this.handleChange.bind(this)} style={style2} placeholder='Full Name'/>
+                <br />
+                <input type="text" name="emailInput" value={this.state.emailInput} onChange={this.handleChange.bind(this)} style={style2} placeholder='Email'/>
+                <br />
+                <textarea type="text" name="textAreaName" value={this.state.textAreaName} onChange={this.handleChange.bind(this)} style={style1} placeholder='Send me a message!!!' />
+                <div>
+                    <button onClick={this.handleSubmit.bind(this)}>Send</button>
+                </div>
+                <Prompt when={this.state.value !==""} message="Are you sure you don't wanna send me a message? :(" />
             </div>
-        </div>
-    )
+        );
+    }
 }
+
+export default Contact;
