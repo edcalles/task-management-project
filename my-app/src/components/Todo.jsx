64ea6a91b52e8bb84f76b292a237996e7ce9e4d6
Edward Calles <ed.calles00@gmail.com>
2023-05-25 15:24:43 -0500

    created task management/switched some colors

--- my-app/src/components/Todo.jsx
+++ my-app/src/components/Todo.jsx
@@ -1,14 +1,202 @@
-import React from 'react'
+import React, { useState } from "react";
+ 
+const Todo = () => {
+ const [showForm, setshowform] = useState(true);
+ const [showNew, setshowNew] = useState(true);
+ const [showDelete, setshowDelete] = useState(true);
+ const [toggleSubmit, settoggleSubmit] = useState(true);
+ const [isEditItem, setisEditItem] = useState(null);
+ const [showList, setshowList] = useState(true);
+ const [editMessage, seteditMessage] = useState(false);
+ const [deleteMessage, setdeleteMessage] = useState(false);
+ const [deleteMessagesuccess, setdeleteMessagesuccess] = useState(false);
+ const [inputTitle, setinputTitle] = useState("");
+ const [inputDesc, setinputDesc] = useState("");
+ const [items, setitems] = useState([
+   {
+     id: "001",
+     name: "Default Task",
+     desc: "Default Description",
+     status: false,
+   },
+ ]);
+ 
+ const handleInput = (e) => {
+   setinputTitle(e.target.value);
+ };
+ const handleInputdesc = (e) => {
+   setinputDesc(e.target.value);
+ };
+ const handleClick = e => {
+    if (e.target.textDecoraction) {
+        e.target.removeProperty('text-decoration');
+    } else {
+        e.target.setProperty('text-decoration', 'line-through')
+    }
+ }
+ const handleSubmit = (e) => {
+   setshowList(true);
+   setshowNew(true);
+ 
+   e.preventDefault();
+   if (!inputTitle || !inputDesc) {
+     alert("you have to fill in the data!!");
+   } else if (inputTitle && !toggleSubmit) {
+     setitems(
+       items.map((elem) => {
+         if (elem.id === isEditItem) {
+           return { ...elem, name: inputTitle, desc: inputDesc };
+         }
+         return elem;
+       })
+     );
+ 
+     setinputTitle("");
+     setinputDesc("");
+     settoggleSubmit(true);
+     setshowform(false);
+     setshowDelete(true);
+   } else {
+     const allinputTitle = {
+       id: new Date().getTime().toString(),
+       name: inputTitle,
+       desc: inputDesc,
+     };
+     setitems([allinputTitle, ...items]);
+     setinputTitle("");
+     setinputDesc("");
+     setshowform(false);
+   }
+ };
 
-const Todo = ()=>{
-    return (
-        <div>
-            <h3>To Do List!</h3>
-            <div>
-                Make a silly list here!
-            </div>
-        </div>
-    )
-}
+ const handleDelete = (index) => {
+   console.log(index);
+   const updatedItems = items.filter((elem) => {
+     return index !== elem.id;
+   });
+   setdeleteMessage(true);
+ 
+   setTimeout(() => {
+     setitems(updatedItems);
+     setdeleteMessage(false);
+   }, 2000);
+   setdeleteMessagesuccess(false);
+ };
 
+ const handleEdit = (id) => {
+   setshowList(false);
+   setshowDelete(false);
+   setshowNew(false);
+   setshowform(true);
+ 
+   settoggleSubmit(false);
+   let newEditItem = items.find((elem) => {
+     return elem.id === id;
+   });
+   setinputTitle(newEditItem.name);
+   setinputDesc(newEditItem.desc);
+ 
+   setisEditItem(id);
+   console.log(newEditItem);
+ };
+
+ const handleAdd = () => {
+   setshowform(true);
+   setshowList(true);
+   setshowNew(false);
+ };
+ return (
+   <>
+     {showNew ? (
+       <div>
+         <div>
+           <button onClick={handleAdd}>
+             Create new task!
+           </button>
+         </div>
+       </div>
+     ) : (
+       ""
+     )}
+ 
+     {showForm ? (
+       <>
+         <div>
+           <div>
+             <div>
+               <h2>{toggleSubmit ? "Add Task" : " Edit Task"}</h2>
+             </div>
+             <form onSubmit={handleSubmit}>
+               <label>Enter Title</label>
+               <input
+                 type="text"
+                 name="title"
+                 id="title"
+                 placeholder="title"
+                 onChange={handleInput}
+                 value={inputTitle}
+               />
+               <br />
+               <label>Description!</label>
+               <input
+                 type="text"
+                 name="description"
+                 id="description"
+                 placeholder="Description"
+                 onChange={handleInputdesc}
+                 value={inputDesc}
+               />
+               <br />
+               {toggleSubmit ? (
+                 <button>Save</button>
+               ) : (
+                 <button>Update</button>
+               )}
+             </form>
+           </div>
+         </div>
+       </>
+     ) : (
+       ""
+     )}
+ 
+     {showList ? (
+       <div>
+         {deleteMessage ? (
+           <p >Item Deleted Successfully</p>
+         ) : (
+           ""
+         )}
+         {items.map((elem) => {
+           return (
+             <div key={elem.id}>
+               <div>
+                 <div>
+                   <h4>{elem.name}</h4>
+                   <p>{elem.desc}</p>
+                 </div>
+                   <button onClick={() => handleEdit(elem.id)}>
+                     Edit
+                   </button>
+                   {showDelete ? (
+                     <button onClick={() => handleDelete(elem.id)}>
+                       Delete
+                     </button>
+                   ) : (
+                     ""
+                   )}
+                   <button onClick={() => handleClick(elem.id)}>Done!</button>
+                 </div>
+               </div>
+            
+           );
+         })}
+       </div>
+     ) : (
+       ""
+     )}
+   </>
+ );
+};
+ 
 export default Todo;
